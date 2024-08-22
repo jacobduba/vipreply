289d7757bce0689503b3dffb73c09986e4c3c6ca
Jacob Duba <jacob@mooshoe.me>
2024-08-21 22:37:06 -0500

    Go back to docker

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,3 @@
 class ApplicationController < ActionController::Base
-  # http_basic_authenticate_with name: "demo", password: "emails"
+  http_basic_authenticate_with name: "demo", password: "emails"
 end
