36f25dda367a53269740e6e48dc5bfa02735f4d8
Jacob Duba <jacob@mooshoe.me>
2024-10-21 14:17:38 -0500

    Get working?

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,22 +7,22 @@ class SessionsController < ApplicationController
     end
 
     @account = Account.new
-    render 'login'
   end
 
   def create
     params_n = login_params
 
-    @username = params_n[:username]
-
-    @account = Account.find_by(username: @password)
+    @account = Account.find_by(username: params_n[:username])
 
     if @account && @account.authenticate(params_n[:password])
       session[:account_id] = @account.id
       redirect_to root_path
     else
-      flash[:alert] = "Username or password is invalid."
-      redirect_to login_path
+      @invalid_username_or_password = true
+      @username = params_n[:username]
+      @account = Account.new
+
+      render :new, status: :unprocessable_entity
     end
   end
 
