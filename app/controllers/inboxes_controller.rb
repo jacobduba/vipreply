1389f558aad87fd2a8aee4c75b8bdefe62ef0f14
Jacob Duba <jacob@mooshoe.me>
2025-01-16 18:33:21 -0600

    Try to speed up loading inbox

--- app/controllers/inboxes_controller.rb
+++ app/controllers/inboxes_controller.rb
@@ -1,8 +1,11 @@
 class InboxesController < ApplicationController
   def index
     @inbox = @account.inbox
-    @to_do_topics = @inbox.topics.where(all_taken_care_of: false).order(date: :asc)
-    @done_topics = @inbox.topics.where(all_taken_care_of: true).order(date: :desc)
+
+    selected_fields = [:id, :snippet, :date, :subject, :from, :to, :all_taken_care_of, :message_count]
+
+    @to_do_topics = @inbox.topics.select(selected_fields).where(all_taken_care_of: false).order(date: :asc)
+    @done_topics = @inbox.topics.select(selected_fields).where(all_taken_care_of: true).order(date: :desc)
   end
 
   def update
