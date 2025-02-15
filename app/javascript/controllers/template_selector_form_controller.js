3b34a2c58648ab447672259ce9b7f174a21e2fb1
Jacob Duba <jacob@mooshoe.me>
2025-02-15 13:23:33 -0600

    Highlight entire template when selecting

--- app/javascript/controllers/template_selector_form_controller.js
+++ app/javascript/controllers/template_selector_form_controller.js
@@ -1,7 +1,14 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from "@hotwired/stimulus";
 
 // Connects to data-controller="template-selector-form"
 export default class extends Controller {
-  connect() {
+  static targets = ["templateCheckbox", "templateDiv"];
+
+  toggleCheck(event) {
+    const checkbox = event.target;
+    const row = checkbox.closest(
+      '[data-template-selector-form-target="templateRow"]',
+    );
+    row.dataset.isChecked = checkbox.checked;
   }
 }
