4d06d16793d52049824dc0a8b898990e09ecb30c
Dakota Rossi <dkgamer8@gmail.com>
2025-02-23 22:18:50 -0600

    Refactor generator concern methods, update inbox job to filter threads, and remove examples controller test

--- app/controllers/concerns/generator_concern.rb
+++ app/controllers/concerns/generator_concern.rb
@@ -3,37 +3,30 @@
 module GeneratorConcern
   extend ActiveSupport::Concern
 
-  # Generate Reply button
+  # Generate Reply button handler
   def handle_regenerate_reply(topic)
     topic.generate_reply
     topic.save!
     render turbo_stream: [
-      turbo_stream.replace("generated_reply_form", partial: "topics/generated_reply_form", locals: {topic: topic, generated_reply: topic.generated_reply}),
-      turbo_stream.replace("template_form", partial: "topics/template_form", locals: {
-        input_errors: [],
-        output_errors: [],
-        topic: topic
-      })
+      turbo_stream.replace("generated_reply_form",
+        partial: "topics/generated_reply_form",
+        locals: {topic: topic, generated_reply: topic.generated_reply}),
+      turbo_stream.replace("template_form",
+        partial: "topics/template_form",
+        locals: {input_errors: [], output_errors: [], topic: topic})
     ]
   end
 
-  # Find Templates button
+  # Find Templates button handler
   def handle_find_templates(topic)
-    message = topic.messages.order(date: :desc).first
-
-    best_templates = message ? Example.find_best_templates(message, topic.inbox) : []
-
-    topic.templates = best_templates
-    topic.save!
-
+    topic.find_best_templates
     render turbo_stream: [
-      turbo_stream.replace("template_form", partial: "templates/template_form", locals: {
-        input_errors: [],
-        output_errors: [],
-        topic: topic,
-        template: (topic.templates.any? ? topic.templates.first : Template.new),
-        show_delete: (topic.templates.any? ? topic.templates.first.persisted? : false)
-      })
+      turbo_stream.replace("template_form",
+        partial: "topics/template_form",
+        locals: {input_errors: [], output_errors: [], topic: topic}),
+      turbo_stream.replace("template-selector-body",
+        partial: "topics/template_selector_body",
+        locals: {templates: topic.templates})
     ]
   end
 end
