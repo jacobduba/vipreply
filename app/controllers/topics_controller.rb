d67d3c9389d42b7d37a815802233c202ee5ab95e
Jacob Duba <jacob@mooshoe.me>
2025-03-02 22:27:23 -0600

    Logging

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -169,7 +169,7 @@ class TopicsController < ApplicationController
     valid_templates = @account.inbox.templates.where(id: template_ids)
 
     if valid_templates.count != template_ids.size || @account != @topic.inbox.account
-      Rails.logger.error("#{@account.email}: Invalid template IDs or account mismatch. Template IDs: #{template_ids.inspect}, Valid Templates: #{valid_templates.inspect}")
+      Rails.logger.error("#{@account.email}: Invalid template IDs or account mismatch. Template IDs: #{template_ids}, Valid Templates: #{valid_templates}")
       render file: "#{Rails.root}/public/404.html", status: :not_found
       return
     end
