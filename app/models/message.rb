5e14b3a90da6162bbb9fc55702238f10825a98ff
Jacob Duba <jacob@mooshoe.me>
2025-06-20 22:49:22 -0500

    Remove content security issue

--- app/models/message.rb
+++ app/models/message.rb
@@ -40,7 +40,6 @@ class Message < ApplicationRecord
 
     <<~HTML
       <head>
-        <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet">
