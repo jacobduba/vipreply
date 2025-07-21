b0c568cc51a87410fb6ca3bd4ba40ecc17a30eff
Jacob Duba <jacob@mooshoe.me>
2025-07-21 15:19:22 -0500

    Found reason for bug in webhooks (invoice objects are different in creation/update), cleaning up the webhooks file since Vibe Coding your payments isn't very good.

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -20,19 +20,22 @@ class WebhooksController < ApplicationController
       end
     end
 
-    case event["type"]
-    when "checkout.session.completed"
-      handle_checkout_completed(event["data"]["object"])
-    when "invoice.payment_succeeded"
-      handle_payment_succeeded(event["data"]["object"])
+    type = event["type"]
+    object = event["data"]["object"]
+
+    Rails.error.set_context(type: type, object: object)
+
+    case type
+    when "invoice.paid"
+      handle_invoice_paid(object)
     when "invoice.payment_failed"
-      handle_payment_failed(event["data"]["object"])
+      handle_payment_failed(object)
     when "customer.subscription.updated"
-      handle_subscription_updated(event["data"]["object"])
+      handle_subscription_updated(object)
     when "customer.subscription.deleted"
-      handle_subscription_deleted(event["data"]["object"])
+      handle_subscription_deleted(object)
     else
-      Rails.logger.info "Unhandled event type: #{event["type"]}"
+      Rails.logger.info "Unhandled event type: #{type}"
     end
 
     render json: {status: "success"}
@@ -40,38 +43,13 @@ class WebhooksController < ApplicationController
 
   private
 
-  def handle_checkout_completed(session)
-    customer_id = session["customer"]
-    subscription_id = session["subscription"]
-    account = Account.find_by!(stripe_customer_id: customer_id)
-
-    # Fetch subscription to get period end
-    subscription = Stripe::Subscription.retrieve(subscription_id)
-    period_end = subscription.items.data.first.current_period_end
-
-    account.update!(
-      stripe_status: subscription.status,
-      stripe_subscription_id: subscription_id,
-      subscription_period_end: Time.at(period_end),
-      cancel_at_period_end: false
-    )
-
-    # Setup inbox for first-time subscribers, import for returning users
-    if account.inbox.history_id.present?
-      # Returning user - import emails since last history_id
-      UpdateFromHistoryJob.perform_later(account.inbox.id)
-    else
-      # First-time user - setup Gmail watch and get initial history_id
-      SetupInboxJob.perform_later(account.id)
-    end
-  end
-
-  def handle_payment_succeeded(invoice)
+  # We can assume they are paying for the One Subscription
+  # that VIPReply offers
+  def handle_invoice_paid(invoice)
+    debugger
     customer_id = invoice["customer"]
     subscription_id = invoice["subscription"]
     account = Account.find_by!(stripe_customer_id: customer_id)
-
-    Rails.error.set_context(invoice: invoice, customer_id: customer_id, subscription_id: subscription_id)
 
     # Fetch subscription to get updated period end
     subscription = Stripe::Subscription.retrieve(subscription_id)
@@ -82,6 +60,15 @@ class WebhooksController < ApplicationController
       stripe_subscription_id: subscription_id,
       subscription_period_end: Time.at(period_end)
     )
+
+    # Setup inbox for first-time subscribers, import for returning users
+    if account.inbox.history_id.present?
+      # Returning user - import emails since last history_id
+      UpdateFromHistoryJob.perform_later(account.inbox.id)
+    else
+      # First-time user - setup Gmail watch and get initial history_id
+      SetupInboxJob.perform_later(account.id)
+    end
   end
 
   def handle_payment_failed(invoice)
