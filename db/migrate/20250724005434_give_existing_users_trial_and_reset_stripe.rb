1d42ca49eab13e1e30d695328bd2b6c09be3c87f
Jacob Duba <jacob@mooshoe.me>
2025-07-23 20:05:12 -0500

    ah

--- db/migrate/20250724005434_give_existing_users_trial_and_reset_stripe.rb
+++ db/migrate/20250724005434_give_existing_users_trial_and_reset_stripe.rb
@@ -3,7 +3,7 @@ class GiveExistingUsersTrialAndResetStripe < ActiveRecord::Migration[8.0]
     # Give all existing users a 30-day trial starting today
     # and reset ALL Stripe info for EVERYONE
     Account.update_all(
-      billing_status: 'trialing',
+      billing_status: "trialing",
       subscription_period_end: 30.days.from_now,
       stripe_customer_id: nil,
       stripe_subscription_id: nil,
