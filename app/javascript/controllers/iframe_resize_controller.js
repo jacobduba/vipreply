88710b336ce7724817c0465d48c8b851ee094d28
Jacob Duba <jacob@mooshoe.me>
2025-09-27 19:45:10 -0500

    Good enough

--- app/javascript/controllers/iframe_resize_controller.js
+++ app/javascript/controllers/iframe_resize_controller.js
@@ -3,51 +3,50 @@ import { Controller } from "@hotwired/stimulus";
 // Iframes need h-0 class to reset height before measuring scrollHeight
 // iframes's inner html expands to max size if less, so html scrollheight will be too big
 export default class extends Controller {
-  static targets = ["iframe", "lastMessage"];
-
   connect() {
-    this.resizeAllIframes();
-    window.addEventListener("resize", this.resizeAllIframes);
-    document.addEventListener("turbo:morph", this.resizeAllIframes);
+    this.resizeIframe();
+    window.addEventListener("resize", this.resizeIframe);
+    document.addEventListener("turbo:morph", this.handleMorph);
 
-    this.iframeTargets.forEach((iframe) => {
-      iframe.addEventListener("load", this.handleIframeLoad);
+    this.element.addEventListener("load", this.handleLoad);
 
-      if (iframe.contentWindow?.document.readyState === "complete") {
-        this.resizeIframe(iframe);
-        this.scrollToLastMessage();
-      }
-    });
+    if (this.element.contentWindow?.document.readyState === "complete") {
+      this.resizeIframe();
+      this.scrollToTarget();
+    }
   }
 
   disconnect() {
-    window.removeEventListener("resize", this.resizeAllIframes);
-    document.removeEventListener("turbo:morph", this.resizeAllIframes);
-    this.iframeTargets.forEach((iframe) => {
-      iframe.removeEventListener("load", this.handleIframeLoad);
-    });
+    window.removeEventListener("resize", this.resizeIframe);
+    document.removeEventListener("turbo:morph", this.handleMorph);
+    this.element.removeEventListener("load", this.handleLoad);
   }
 
-  handleIframeLoad = (event) => {
-    this.resizeIframe(event.target);
-    this.scrollToLastMessage();
+  handleLoad = () => {
+    this.resizeIframe();
+    this.scrollToTarget();
   };
 
-  resizeAllIframes = () => {
-    this.iframeTargets.forEach(this.resizeIframe);
+  handleMorph = () => {
+    // Reset height before iframe reloads with new content
+    this.element.style.height = '0px';
+    setTimeout(() => this.resizeIframe(), 100);
   };
 
-  resizeIframe = (iframe) => {
-    const html = iframe.contentWindow?.document?.documentElement;
+  resizeIframe = () => {
+    const html = this.element.contentWindow?.document?.documentElement;
     if (!html) return;
 
     // Add 2px to account for border/padding
-    iframe.style.height = `${html.scrollHeight}px`;
+    this.element.style.height = `${html.scrollHeight}px`;
   };
 
-  scrollToLastMessage = () => {
+  scrollToTarget = () => {
+    const target = document.getElementById("last-message");
+    if (!target) return;
+
     const offset = 80;
-    const rect = this.lastMessageTarget.getBoundingClientRect();
+    const rect = target.getBoundingClientRect();
     window.scrollTo({
       top: rect.top + window.scrollY - offset,
     });
