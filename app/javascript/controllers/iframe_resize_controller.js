f280a2e267e0cbd8c050c0e394fef95d3cc92efc
Jacob Duba <jacob@mooshoe.me>
2025-09-27 21:27:41 -0500

    Okay rails turbo isn't so bad. Just not an abstraction.

--- app/javascript/controllers/iframe_resize_controller.js
+++ app/javascript/controllers/iframe_resize_controller.js
@@ -6,8 +6,6 @@ export default class extends Controller {
   connect() {
     this.resizeIframe();
     window.addEventListener("resize", this.resizeIframe);
-    document.addEventListener("turbo:morph", this.handleMorph);
-
     this.element.addEventListener("load", this.handleLoad);
 
     if (this.element.contentWindow?.document.readyState === "complete") {
@@ -18,19 +16,12 @@ export default class extends Controller {
 
   disconnect() {
     window.removeEventListener("resize", this.resizeIframe);
-    document.removeEventListener("turbo:morph", this.handleMorph);
     this.element.removeEventListener("load", this.handleLoad);
   }
 
   handleLoad = () => {
     this.resizeIframe();
     this.scrollToTarget();
-  };
-
-  handleMorph = () => {
-    // Reset height before iframe reloads with new content
-    this.element.style.height = '0px';
-    setTimeout(() => this.resizeIframe(), 100);
   };
 
   resizeIframe = () => {
@@ -42,13 +33,14 @@ export default class extends Controller {
   };
 
   scrollToTarget = () => {
-    const target = document.getElementById("last-message");
+    const target = document.querySelector("[is-last-vipreply-message]");
     if (!target) return;
 
     const offset = 80;
     const rect = target.getBoundingClientRect();
     window.scrollTo({
       top: rect.top + window.scrollY - offset,
+      behavior: "smooth",
     });
   };
 }
