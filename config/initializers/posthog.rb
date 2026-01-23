b5aa38d8ea64b244ab54992a6234016a8ac1dad9
Jacob Duba <jacob@mooshoe.me>
2026-01-23 16:38:36 -0600

    Skip PostHog init during asset precompilation

--- config/initializers/posthog.rb
+++ config/initializers/posthog.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
+# Skip during asset precompilation (credentials aren't available during Docker build)
+return if ENV["SECRET_KEY_BASE_DUMMY"].present?
+
 POSTHOG = PostHog::Client.new(
   api_key: Rails.application.credentials.posthog_api_key,
   host: "https://e.vipreply.ai",
